8ed359119cd2080794f891718a7a1077ae1a199d
Franklin Makono <franklin@t450s.lan>
2023-09-30 23:49:44 +0200

    first commit

---
+++ questions_defaut.js
@@ -0,0 +1,62 @@
+export const Questions = [
+  {
+    question:
+      "Quel service AWS est principalement utilisé pour le calcul dans le cloud ?",
+    answers: ["Amazon S3", "Amazon EC2", "Amazon RDS"],
+    correct: "Amazon EC2",
+  },
+  {
+    question:
+      "Dans quelle AWS service mettriez-vous du code sans avoir à gérer les serveurs sous-jacents ?",
+    answers: ["Amazon EC2", "Amazon RDS", "AWS Lambda"],
+    correct: "AWS Lambda",
+  },
+  {
+    question: "Quel est le service AWS pour le stockage objet ?",
+    answers: ["Amazon Redshift", "AWS Lambda", "Amazon S3"],
+    correct: "Amazon S3",
+  },
+  {
+    question: "Quelle base de données NoSQL est proposée par AWS ?",
+    answers: ["DynamoDB", "Aurora", "RDS SQL Server"],
+    correct: "DynamoDB",
+  },
+  {
+    question:
+      " Quel outil permet la gestion et le déploiement d'infrastructures sous forme de code ?",
+    answers: ["AWS CloudFormation", "AWS CloudWatch", "AWS CloudTrail"],
+    correct: "AWS CloudFormation",
+  },
+  {
+    question: "Quel est le service de surveillance d'AWS ?",
+    answers: ["AWS CloudFormation", "AWS CloudWatch", "AWS CloudTrail"],
+    correct: "AWS CloudWatch",
+  },
+  {
+    question:
+      "Quel service utilisez-vous pour distribuer le trafic entrant à plusieurs cibles, telles que des instances Amazon EC2 ?",
+    answers: ["AWS VPC", "Amazon EBS", "Amazon Elastic Load Balancer (ELB)"],
+    correct: "Amazon Elastic Load Balancer (ELB)",
+  },
+  {
+    question: "Que signifie EC2 dans Amazon EC2 ?",
+    answers: [
+      "Encrypted Computing Cluster",
+      "Elastic Compute Cloud",
+      "Extended Cloud Computing",
+    ],
+    correct: "Elastic Compute Cloud",
+  },
+  {
+    question:
+      "Si vous voulez déployer une application Web sans gérer l'infrastructure sous-jacente, quel service AWS pourriez-vous utiliser ?",
+    answers: ["Amazon RDS", "Amazon EC2", "AWS Elastic Beanstalk"],
+    correct: "AWS Elastic Beanstalk",
+  },
+  {
+    question:
+      "Pour déployer une application à l'échelle mondiale avec une faible latence pour vos utilisateurs, lequel des services suivants pouvez-vous utiliser pour distribuer le contenu ?",
+    answers: ["AWS Lambda", "Amazon EC2", "Amazon CloudFront"],
+    correct: "Amazon CloudFront",
+  },
+];
